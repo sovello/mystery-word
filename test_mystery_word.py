e68d6581cb466c736411b89305a2d0ecafd2c184
sovello <sovellohpmgani@gmail.com>
2015-05-08 16:09:41 -0400

    mystery_word.py

--- test_mystery_word.py
+++ test_mystery_word.py
@@ -5,17 +5,17 @@ word_list = ["bird", "calf", "river", "stream", "kneecap",  "cookbook",
 
 
 def test_easy_words():
-    assert easy_words(word_list) == \
+    assert get_easy_words(word_list) == \
         ["bird", "calf", "river", "stream", "brain"]
 
 
 def test_medium_words():
-    assert medium_words(word_list) == \
+    assert get_medium_words(word_list) == \
         ["stream", "kneecap", "cookbook", "language", "sneaker"]
 
 
 def test_hard_words():
-    assert hard_words(word_list) == \
+    assert get_hard_words(word_list) == \
         ["cookbook", "language", "algorithm", "integration"]
 
 
@@ -23,7 +23,7 @@ def test_random_word():
     """This test is not very good. Testing things that are random is hard, in
     that there's not a predictable choice. The best we can do is make sure
     we have valid output."""
-    word = random_word(word_list)
+    word = pick_random_word(word_list)
     assert word in word_list
 
 
